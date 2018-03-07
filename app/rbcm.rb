6ceb791e884e0c83c9ca474a64c86bf50f4c66e4
mwie <mwiegand@seibert-media.net>
2018-03-07 01:19:32 +0100

    random commit

--- app/rbcm.rb
+++ app/rbcm.rb
@@ -1,14 +1,25 @@
 class RBCM
   def initialize
     @nodes = {}
+    # collects jobs from nodes with regex patterns to be apllied after all nodes are collected
+    @patterns = {}
+    Dir["..config/nodes/**/*.rb"].each do |file| 
+      require file
+    end
   end
 
   def node names
+    job = Proc.new # Proc.new without paramaters catches the given block
     [names].flatten.each do |name|
-      @nodes[name] = Node.new name unless @nodes[node_name]
-      # Proc.new without paramaters catches the given block
-      @nodes[name].add_job Proc.new
+      pattern name, job and continue if node_name.class == Rebexp
+      @nodes[name] = Node.new name unless @nodes[name]
+      @nodes[name].add_job job
     end
+  end
+
+  def pattern node_name, job
+    @patterns[node_name] = [] unless @patterns[node_name]
+    @patterns[node_name] << job
   end
 
   def apply
