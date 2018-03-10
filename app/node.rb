c4cbb393d20ed289e23b0622b87e815f38593d5c
mwie <mwiegand@seibert-media.net>
2018-03-10 12:15:20 +0100

    0.0.1

--- app/node.rb
+++ app/node.rb
@@ -24,8 +24,6 @@ class Node
         File.write self, content
       end
     end
-    # manipulations are added to
-
     # commands are placed in file
     File.write "#{@cache_path}.sh", (@manipulations+@commands).join("\n")
   end
