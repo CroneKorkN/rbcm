b44ad2114b411f4b81c2dba0573d85777bad7ae2
mwie <mwiegand@seibert-media.net>
2018-03-10 08:49:06 +0100

    random commit

--- app/1.rb
+++ app/1.rb
@@ -1,11 +1,18 @@
 
 class C
-  def initialize
+  def self.init
+    cached_methods = private_methods
+    p private_methods.sort.count
     load "f.rb"
-    function_name = "f"
+    f
+    p private_methods.sort.count
+    p function_name = (private_methods - cached_methods).first.to_sym
+    p
     method = lambda(&method(function_name.to_sym))
-    self.define_singleton_method function_name, &method
+    self.define_method function_name, &method
   end
 end
+
+C.init
 
 C.new.f
--- app/node.rb
+++ app/node.rb
@@ -2,9 +2,9 @@ class Node
   def initialize name
     @name = name
     @jobs = [] # lambdas from node files
-    @commands = [] # strings, run as root
     @files = {} # path: "content"
     @manipulations = [] # array of commands for manipulating files
+    @commands = [] # strings, run as root
   end
 
   def add_job job
@@ -27,9 +27,10 @@ class Node
     @files[path] = content if content or exists
     @commands << "chmod #{mode}" if mode
     @commands << "
-      if ! grep -q #{includes_line} #{path}
-      then echo #{includes_line} >> #{path}
-    " if includes_line
+      if ! grep -q #{includes_line} #{path}; then
+        echo #{includes_line} >> #{path}
+      fi
+      " if includes_line
 
   end
 
@@ -41,13 +42,15 @@ class Node
 
   def self.load_capabilities
     Dir['../config/capabilities/*.rb'].each do |path|
+      methods_cached = methods
       load path
-      function_name = File.basename path, '.rb'
-      
-      puts function_name
-      function = lambda(&method(function_name.to_sym))
-      puts function
-      #self.define_method function_name, lambda(&method(function_name.to_sym))
+      function_name = (methods - methods_cached).to_s
+      p path
+      p methods_cached.sort
+      p methods.sort
+      puts function_name.to_sym
+      method = lambda(&method(function_name.to_sym))
+      self.define_method function_name, &method
     end
   end
 end
