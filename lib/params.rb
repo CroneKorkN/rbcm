b631202092b58f4fb97e12c87c42422d87a107ca
mwie <mwiegand@seibert-media.net>
2018-05-06 15:50:40 +0200

    random commit

--- lib/params.rb
+++ lib/params.rb
@@ -1,7 +1,16 @@
-class Params < Array
+class Params
   attr_reader :ordered, :named
 
   def initialize ordered, named
     @ordered, @named = ordered, named
+  end
+
+  def [] id
+    ordered[id] if id.class == Integer
+    named[id] if id.class == Symbol
+  end
+
+  def to_s
+    [ordered, named.collect{|k,v| "#{k}: #{v}"}].join(', ')
   end
 end
--- test.rb
+++ test.rb
@@ -1,9 +1,21 @@
 #!/usr/bin/env ruby
 
-def f *oargs, **kargs
-  p oargs
-  p kargs
-  p [*oargs, kargs]
+class Params
+  attr_reader :ordered, :named
+
+  def initialize ordered, named
+    @ordered, @named = ordered, named
+  end
+
+  def [] id
+    return ordered[id] if id.class == Integer
+    return named[id] if id.class == Symbol
+  end
+
+  def to_s
+    [ordered, named.collect{|k,v| "#{k}: #{v}"}].join(', ')
+  end
 end
 
-f "hallo", {ja: "nein"}, ja: "nein"
+p = Params.new ["hallo", "thuss"], {petra: "musetrmann", '3': 4}
+p p[:'3']
