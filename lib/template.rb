5a0e021d77f294f41e7277574a6e86ed14bdfb1c
mwie <mwiegand@seibert-media.net>
2018-05-07 18:51:31 +0200

    multilayer templates

--- lib/template.rb
+++ lib/template.rb
@@ -2,14 +2,43 @@ class Template
   @@engines = [:mustache]
 
   def initialize project_path, capability, template_name, context: {}
+    @project_path = project_path
+    @capability = capability
+    @template_name = template_name
     @context = context
-    Dir["#{project_path}/capabilities/#{capability.to_s.gsub("!","")}/**/*"].each do |path|
-      @content = File.read(path) if File.basename(path).gsub(".mustache", "") == template_name
-    end
-    raise "no file found for template '#{template_name}'" unless @content
   end
 
   def render
-    Mustache.render(@content, **@context)
+    content = File.read path
+    p content
+    p layers
+    layers.each do |layer|
+      if layer == :mustache
+        content = Mustache.render(content, **@context)
+      else
+        raise "RBCM: unknown template engine '#{layer}'"
+      end
+    end
+    return content
+  end
+
+  def path
+    @path ||= Dir["#{@project_path}/capabilities/#{@capability.to_s.gsub("!","")}/#{@template_name}*"].first
+  end
+
+  def filename
+    File.basename(path)
+  end
+
+  def layers
+    @layers = []
+    filename.split(".").reverse.each.to_sym.each do |layer|
+      if @@engines.include? layer
+        @layers << layer
+      else
+        break
+      end
+    end
+    return @layers
   end
 end
