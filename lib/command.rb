a7a6bac72d3a63d35568b3f84c0173a2a8e159ca
mwie <mwiegand@seibert-media.net>
2018-05-20 18:24:45 +0200

    random commit

--- lib/command.rb
+++ lib/command.rb
@@ -9,10 +9,6 @@ class Command < Action
     end
   end
 
-  def diff
-    "$>_ \e[1m#{@line}\e[0m\e[2m#{" CHECK " if @check}#{@check}\e[0m"
-  end
-
   def siblings
     @node.rbcm.actions.select{ |action|
       action.chain[1..-1] == @chain[1..-1] and action.line == @line
