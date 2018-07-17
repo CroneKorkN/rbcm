0202540b1ffcdc0b53794a0c40caed5acc7f656b
mwie <mwiegand@seibert-media.net>
2018-07-17 23:33:12 +0200

    working on template dirs

--- app/node/sandbox.rb
+++ app/node/sandbox.rb
@@ -116,9 +116,9 @@ class Node::Sandbox
   end
 
   def dir templates:
-    @node.rbcm.project.templates.collect do |template|
+    @node.rbcm.project.templates.collect{ |template|
 
-    end
+    }
   end
 
   def decrypt secret
@@ -207,7 +207,8 @@ class Node::Sandbox
         @node.triggered.append capability.name
         r = __cache trigger: params[:trigger],
               triggered_by: params[:triggered_by],
-              chain: capability.name do
+              chain: capability.name
+        do
           send "__#{__method__}", *params.delete(:trigger, :triggered_by).sendable
         end
         @dependency_cache = [:file]
