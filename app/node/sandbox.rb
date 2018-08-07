51c9f66e0ee88b995865716f575963c49176c81d
mwie <mwiegand@seibert-media.net>
2018-08-07 21:34:43 +0200

    random commit

--- app/node/sandbox.rb
+++ app/node/sandbox.rb
@@ -120,9 +120,10 @@ class RBCM::Node::Sandbox
   end
 
   def dir path="", templates:, context: {}, tags: nil, trigger: nil, triggered_by: nil
+    templates.gsub! /!^/, ''
     __cache tags: tags, trigger: trigger, triggered_by: triggered_by, working_dirs: working_dir do
       @node.rbcm.project.templates.under("#{working_dir}/#{templates}").each do |template|
-        file template.clean_full_path.gsub(/#{working_dir}/),
+        file template.clean_full_path.gsub(/^#{working_dir}/, '').gsub(/^\/#{templates}/, ''),
           template: template.clean_path,
           context: context
       end
