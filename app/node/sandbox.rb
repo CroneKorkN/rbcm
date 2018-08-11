8b9bfe4233c42562e1327b26429ab03076668363
mwie <mwiegand@seibert-media.net>
2018-08-11 19:52:22 +0200

    fix template target path missing prefix

--- app/node/sandbox.rb
+++ app/node/sandbox.rb
@@ -119,11 +119,11 @@ class RBCM::Node::Sandbox
       check: "stat -c '%G' * #{path} | grep -q #{named[:group]}" if named[:group]
   end
 
-  def dir path="", templates:, context: {}, tags: nil, trigger: nil, triggered_by: nil
+  def dir path="/", templates:, context: {}, tags: nil, trigger: nil, triggered_by: nil
     templates.gsub! /\/^/, ''
     __cache tags: tags, trigger: trigger, triggered_by: triggered_by, working_dirs: working_dir do
       @node.rbcm.project.templates.under("#{working_dir}/#{templates}").each do |template|
-        file template.clean_full_path.gsub(/^#{working_dir}/, '').gsub(/^\/#{templates}/, ''),
+        file path + template.clean_full_path.gsub(/^#{working_dir}\/#{templates}/, '').gsub(/^\/#{templates}/, ''),
           template: template.clean_path,
           context: context
       end
