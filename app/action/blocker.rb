eba902880fff1d374be559dbdd8005e87a587531
mwie <mwiegand@seibert-media.net>
2019-02-18 18:59:09 +0100

    better block checks

--- app/action/blocker.rb
+++ app/action/blocker.rb
@@ -5,7 +5,7 @@ class RBCM::Blocker
   end
   
   def reasons
-    @reasons ||= [*triggered_by, *checks]
+    @reasons ||= [*triggered_by, *check].compact
   end
   
   def triggered_by
@@ -16,13 +16,17 @@ class RBCM::Blocker
     }
   end
   
-  def checks
-    @action.job.stack.capability(:check).collect do |job|
-      if @action.node.checks[@action.job.params[:check]] != false
-        result = @action.node.remote.execute(@action.job.params[:check]).exitstatus == 0
-        @action.node.checks[@action.job.params[:check]] = result
+  def check
+    "checks:successfull" if [ 
+      *@action.job.stack.capability(:check),
+      *@action.job.stack.with(:check)
+    ].collect{ |job|
+      check = job.params[:check] || job.params[0]
+      if @action.node.checks[job.hash] != false
+        result = @action.node.remote.execute(job.params[0]).exitstatus == 0
+        @action.node.checks[job.hash] = result
       end
-      "check:#{@action.job.params[:check]}" unless @action.node.checks[@action.job.params[:check]]
-    end
+      @action.node.checks[job.hash]
+    }.all?
   end
 end
