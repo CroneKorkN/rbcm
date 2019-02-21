5d1aa183a16eb43c484e69ddc8d8b157a19f7b11
mwie <mwiegand@seibert-media.net>
2019-02-21 19:58:26 +0100

    rbcm in methods

--- app/rbcm.rb
+++ app/rbcm.rb
@@ -24,54 +24,49 @@ module RBCM
 
     def initialize project_path
       @project_path = project_path
-      
-      ## get jobs
-      
-      @projects = [ 
-        RBCM::Project.new("#{@@app_path}/capabilities"),
-        RBCM::Project.new(project_path),
-      ]
-      @definitions = RBCM::DefinitionList.new @projects.each.definitions.flatten
-      @jobs = RBCM::JobList.new @projects.each.jobs.flatten
-      @actions = RBCM::ActionList.new
-      @env = {
-        rbcm: self,
-        instance_variables: {},
-        class_variables: {},
-        jobs: @jobs,
-        checks: [],
-        actions: @actions,
-        definitions: @definitions
-      }
-      while job = @jobs.status(:new).first || @jobs.status(:delayed).first
-        job.run @env
-      end
-      
-      ## get actions
-      
-      @nodes = RBCM::NodeList.new 
-      @jobs.capability(:node).each do |job| 
-        @nodes[job.params[0]] ||= RBCM::Node.new(
-          rbcm: self, 
-          name: job.params[0]
-        )
-        @nodes[job.params[0]].jobs
-      end
-      @actions = @nodes.collect(&:actions).flatten
-
-      ## print
-      
-      puts "-- projects (#{@projects.count}) --\n#{@projects.collect(&:path)}"
-      puts "-- definitions (#{@definitions.count}) --\n#{@definitions.collect(&:to_s)}"
-      puts "-- jobs (#{@jobs.count}) --"
-      puts @jobs.childless.collect{|job| job.trace.collect(&:to_s).join(" > ")}.join("\n")
-      puts "-- nodes (#{@nodes.count}) --\n#{@nodes.collect(&:name)}"
-      puts "-- actions (#{@actions.count}) --"
-      puts @actions.collect{|action| action.job.trace.collect(&:to_s).join(" > ")}.join("\n")
-
-      #binding.pry
     end
     
-    attr_accessor :actions, :definitions, :jobs, :projects
+    def actions
+      RBCM::ActionList.new nodes.collect(&:actions).flatten
+    end
+    
+    def nodes
+      @nodes ||= RBCM::NodeList.new \
+        jobs.capability(:node).collect{|j| j.params.first}.uniq.collect{ |name| 
+          RBCM::Node.new rbcm: self, name: name
+        }
+    end
+    
+    def jobs
+      unless @jobs
+        @jobs = RBCM::JobList.new projects.each.jobs.flatten
+        @env = {
+          rbcm: self,
+          instance_variables: {},
+          class_variables: {},
+          jobs: @jobs,
+          checks: [],
+          definitions: definitions
+        }
+        while job = @jobs.status(:new).first || @jobs.status(:delayed).first
+          job.run @env
+        end
+      end
+      @jobs
+    end
+    
+    def definitions
+      @definitions ||= RBCM::DefinitionList.new projects.each.definitions.flatten
+    end
+    
+    def projects
+      @projects ||= [ 
+        RBCM::Project.new("#{@@app_path}/capabilities"),
+        RBCM::Project.new(@project_path),
+      ]
+    end
+    
+    attr_accessor :actions
+    attr_writer :projects, :definitions, :jobs
   end
 end
