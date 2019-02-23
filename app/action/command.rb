f3310ed1a0ce71a46b9f823e337bd5201f27454e
mwie <mwiegand@seibert-media.net>
2019-02-23 16:45:11 +0100

    fix command param

--- app/action/command.rb
+++ app/action/command.rb
@@ -6,6 +6,6 @@ class RBCM::Action::Command < RBCM::Action
   # execute the command remote
   def run!
     @applied = true
-    @result = @node.remote.execute(@params[:line])
+    @result = @node.remote.execute(@params.first)
   end
 end
